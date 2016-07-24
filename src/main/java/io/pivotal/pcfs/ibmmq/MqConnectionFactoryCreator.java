16494e4f67dea7c7a99d44d99595f526675b9bff
Alex Shumilov <ashumilov@pivotal.io>
2016-07-24 17:01:24 -0400

    cleanup dependencies

--- src/main/java/io/pivotal/pcfs/ibmmq/MqConnectionFactoryCreator.java
+++ src/main/java/io/pivotal/pcfs/ibmmq/MqConnectionFactoryCreator.java
@@ -11,8 +11,7 @@ import java.util.logging.Logger;
 
 public class MqConnectionFactoryCreator extends AbstractServiceConnectorCreator<MQQueueConnectionFactory, MqServiceInfo> {
     private MqConnectionFactoryConfigurer configurer = new MqConnectionFactoryConfigurer();
-    private static Logger logger = Logger.getLogger(MqServiceInfoCreator.class.getName());
-
+    private static Logger logger = Logger.getLogger(MqConnectionFactoryCreator.class.getName());
 
 
     @Override
@@ -24,12 +23,13 @@ public class MqConnectionFactoryCreator extends AbstractServiceConnectorCreator<
     private MQQueueConnectionFactory createMqConnectionFactory(MqServiceInfo serviceInfo) {
         //todo - follow http://www.massapi.com/class/mq/MQQueueConnectionFactory.html
 //        try {
-            MQQueueConnectionFactory connectionFactory = new MQQueueConnectionFactory();
-            logger.info("****Creating MQ Connection Factory:");
-            logger.info("****Host: " + serviceInfo.getHost());
-            logger.info("****Port: " + serviceInfo.getPort());
-            logger.info("****Queue Manager: " + serviceInfo.getQueueManager());
-            logger.info("****User: " + serviceInfo.getUserName());
+        MQQueueConnectionFactory connectionFactory = new MQQueueConnectionFactory();
+        connectionFactory.setHost(serviceInfo.getHost());
+        connectionFactory.setPort(serviceInfo.getPort());
+        connectionFactory.setQueueManager(serviceInfo.getQueueManager());
+        connectionFactory.setUserName(serviceInfo.getUserName());
+        logger.info("****Creating MQ Connection Factory: " + connectionFactory);
+
 
 //            connectionFactory.setHostName(serviceInfo.getHost());
 //            connectionFactory.setPort(serviceInfo.getPort());
@@ -38,7 +38,7 @@ public class MqConnectionFactoryCreator extends AbstractServiceConnectorCreator<
 //            connectionFactory.setTransportType(JMSC.MQJMS_TP_CLIENT_MQ_TCPIP);
 //            connectionFactory.createQueueConnection().start();
 
-            return connectionFactory;
+        return connectionFactory;
 //        } catch (JMSException e) {
 //            e.printStackTrace();
 //        }
--- src/main/java/io/pivotal/pcfs/ibmmq/mock/MQQueueConnectionFactory.java
+++ src/main/java/io/pivotal/pcfs/ibmmq/mock/MQQueueConnectionFactory.java
@@ -1,5 +1,22 @@
 package io.pivotal.pcfs.ibmmq.mock;
 
 
+import lombok.Data;
+
+@Data
 public class MQQueueConnectionFactory {
+    private String host;
+    private int port;
+    private String queueManager;
+    private String userName;
+
+    @Override
+    public String toString() {
+        return "MQQueueConnectionFactory{" +
+                "host='" + host + '\'' +
+                ", port=" + port +
+                ", queueManager='" + queueManager + '\'' +
+                ", userName='" + userName + '\'' +
+                '}';
+    }
 }
